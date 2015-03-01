a0b91ae3193ae2dd5909b1e16a308c15b9890fe6
liuyulong <yulongliu123@hotmail.com>
2015-03-01 15:39:46 +0800

    追加Path的简单使用，绘制自定义窗口

---
+++ WpfAppStart/WindowUserPath.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppStart
+{
+    /// <summary>
+    /// WindowUserPath.xaml 的交互逻辑
+    /// </summary>
+    public partial class WindowUserPath : Window
+    {
+        public WindowUserPath()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
